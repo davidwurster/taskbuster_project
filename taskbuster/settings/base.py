c9d624ac6cae20d2cd1dedec0236731a2c9e1822
David Wurster <david.wurster@gmail.com>
2018-09-01 17:16:30 -0400

    End of part X

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # TaskBuster apps
+    'taskbuster.apps.taskmanager',
 ]
 
 INSTALLED_APPS += (
